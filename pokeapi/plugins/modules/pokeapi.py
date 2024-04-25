624e096fb397d4dd69a5bf5eb6317203c4ebe536
albertoagss <fp_aguilerajose@ciudadjardin.org>
2024-04-25 08:06:20 +0000

    Comentado código del módulo pokeapi

--- pokeapi/plugins/modules/pokeapi.py
+++ pokeapi/plugins/modules/pokeapi.py
@@ -8,8 +8,11 @@ import requests
 
 # Función para realizar la petición a la Api
 def get_pokemon_data(pokemon_name):
+    # URL para la llamada a la API
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
+    # Lanzamos una petición get a la url y almacenamos su contenido en la variable answer
     answer = requests.get(url)
+    # Si la petición es exitosa (código de estado 200) devolvemos el contenido de la variable en formato json
     if answer.status_code == 200:
         return answer.json()
     else:
@@ -17,19 +20,27 @@ def get_pokemon_data(pokemon_name):
 
 # Función principal
 def main():
+    # Definimos el módulo de Ansible
     module = AnsibleModule(
+        # Definimos los argumentos que acepta el módulo
         argument_spec=dict(
+            # Argumento 'pokemon', siempre requerido
             pokemon=dict(type='str', required=True)
         )
     )
     
+    # Obtenemos el valor del argumento pokemon y lo almacenamos en la variable 'pokemon_name'
     pokemon_name = module.params['pokemon']
+    # Llamamos a la función 'get_pokemon_data'
     pokemon_data = get_pokemon_data(pokemon_name)
     
+    # Si la función 'get_pokemon_data' devuelve datos, salimos del módulo 'module.exit_json'
     if pokemon_data:
         module.exit_json(changed=False, pokemon_data=pokemon_data)
+    # Si la función 'get_pokemon_data' NO devuelve datos, fallamos el módulo con 'module.fail_json'
     else:
         module.fail_json(msg=f"Pokemon {pokemon_name} not found")
         
+# Flujo principal
 if __name__ == '__main__':
     main()
