eb6195216ca24d117c8e26d043a36694d2e40633
albertoagss <fp_aguilerajose@ciudadjardin.org>
2024-04-26 07:25:19 +0000

    Prueba buscar movimientos

--- pokeapi/plugins/modules/pokeapi.py
+++ pokeapi/plugins/modules/pokeapi.py
@@ -4,10 +4,17 @@ from ansible.module_utils.basic import *
 # Importar la biblioteca 'requests', que sirve para realizar solicitudes HTTP
 import requests
 
-# Función para realizar la petición 'BUSCAR POKEMON' a la Api
-def get_pokemon_data(pokemon_name):
+# Función para realizar la petición a la Api
+def get_pokemon_data(name, search_type):
     # URL para la llamada a la API
-    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
+    base_url = f"https://pokeapi.co/api/v2/"
+    if search_type == "pokemon":
+        url = f"{base_url}pokemon/{name.lower()}"
+    elif search_type == "move":
+        url = f"{base_url}move/{name.lower()}" 
+    else:
+        return None
+
     # Lanzamos una petición get a la url y almacenamos su contenido en la variable answer
     answer = requests.get(url)
     # Si la petición es exitosa (código de estado 200) devolvemos el contenido de la variable en formato json
@@ -33,24 +40,25 @@ def main():
     # Definimos el módulo de Ansible
     module = AnsibleModule(
         # Definimos los argumentos que acepta el módulo
-        argument_spec = {
-            'pokemon' : {'type':'str'}, 
-            'move' : {'type':'str'}
-        }
-            # Argumento 'pokemon', siempre requerido
+        argument_spec=dict(
+            name=dict(type='str', required=True),
+            search_type=dict(type='str', required=True, choices=['pokemon', 'move'])
+        )
     )
 
     # Obtenemos el valor del argumento pokemon y lo almacenamos en la variable 'pokemon_name'
-    pokemon_name = module.params['pokemon']
+    name = module.params['name']
+    search_type = module.params['search_type']
+    
     # Llamamos a la función 'get_pokemon_data'
-    pokemon_data = get_pokemon_data(pokemon_name)
+    pokemon_data = get_pokemon_data(name, search_type)
 
     # Si la función 'get_pokemon_data' devuelve datos, salimos del módulo 'module.exit_json'
     if pokemon_data:
         module.exit_json(changed=False, pokemon_data=pokemon_data)
     # Si la función 'get_pokemon_data' NO devuelve datos, fallamos el módulo con 'module.fail_json'
     else:
-        module.fail_json(msg=f"Pokemon {pokemon_name} not found")
+        module.fail_json(msg=f"No {search_type} found wirh name {name}")
 
 # Flujo principal
 if __name__ == '__main__':
